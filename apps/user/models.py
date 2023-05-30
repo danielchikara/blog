638bb7791293f0b9a1f2cbfc2fbac754b5eab9df
danielchikara <brandonnakama@gmail.com>
2023-05-30 11:11:22 -0500

    create user and role model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, related_name= 'users_rol')
+    biography = models.TextField(max_length=250)
+    profile_image = models.ImageField(upload_to='profile_images/')
+    
+    def __str__(self) -> str:
+        return self.username
+    
+    
---
+++ migrate.sh
@@ -0,0 +1,2 @@
+python3 manage.py makemigrations user
+python3 manage.py migrate
